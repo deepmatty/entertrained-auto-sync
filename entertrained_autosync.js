4db9f92c1fd71fea9e2787e5dfc3a6bc672cd3fa
Thu Anh Ma <tam.thuma@gmail.com>
2026-04-17 15:24:38 +0200

    default user script

---
+++ entertrained_autosync.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Automatic sync for Entertrained
+// @namespace    http://tampermonkey.net/
+// @version      2026-04-17
+// @description  try to take over the world!
+// @author       deepmatty
+// @match        *://entertrained.app/*
+// @match        *://*.entertrained.app/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=entertrained.app
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+})();
